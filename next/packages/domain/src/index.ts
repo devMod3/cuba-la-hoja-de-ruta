27c8e55e121c38ff597a20a9457ec872f27b4aa9
Dembe zuma <101433401+devMod3@users.noreply.github.com>
2026-08-23 23:18:33 -0400

    NEXT Q-038: export metadata registry contracts

--- next/packages/domain/src/index.ts
+++ next/packages/domain/src/index.ts
@@ -1,2 +1,11 @@
 export { ArticleSchema, type Article } from './article';
-export { MetadataRecordSchema, type MetadataRecord } from './metadata';
+export {
+  MetadataClassificationSchema,
+  MetadataEditorialSchema,
+  MetadataIndexingSchema,
+  MetadataNormReferenceSchema,
+  MetadataRecordSchema,
+  MetadataRegistrySchema,
+  type MetadataRecord,
+  type MetadataRegistry
+} from './metadata';
